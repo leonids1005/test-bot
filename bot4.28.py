e8dd67e1f6839472129019eab9248dac5dfb1e42
leonids1005 <brian960729@gmail.com>
2023-05-28 18:05:36 +0800

    Create bot4.28.py

---
+++ bot4.28.py
@@ -0,0 +1,21 @@
+import discord
+
+bot = discord.Bot(intents = discord.Intents.all())
+
+bot = commands.Bot(command_prefix='[')
+
+@bot.event
+async def on_ready()
+   print(">>Bot is online<<")
+
+@bot.event
+async def on_member_join(member)
+   channel = bot.get_channel(1086175181229269032)
+   await channel.send(F'[member] join')
+
+@bot.event
+async def on_member_remove(member)
+   channel = bot.get_channel(1086175181229269032)
+   await channel.send(F'[member] leave')
+
+   bot.run(MTA4NjE3MDY5Nzc3NDE0MTQ1MA.G35fy6.JmLajaT2kCdyclopFVDAGeruQCoQsOwPB45wt4)
