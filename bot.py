2412d301f87359294a51100358d8283af15815a5
leonids1005 <brian960729@gmail.com>
2023-05-28 18:42:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,10 @@
 import discord
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+from discord.ext import commands
 
 bot = commands.Bot(command_prefix='[')
 
 @bot.event
-async def on_ready()
+async def on_ready():
    print(">>Bot is online<<")
 
-@bot.event
-async def on_member_join(member)
-   channel = bot.get_channel(1086175181229269032)
-   await channel.send(F'[member] join')
-
-@bot.event
-async def on_member_remove(member)
-   channel = bot.get_channel(1086175181229269032)
-   await channel.send(F'[member] leave')
-
-   bot.run(MTA4NjE3MDY5Nzc3NDE0MTQ1MA.G35fy6.JmLajaT2kCdyclopFVDAGeruQCoQsOwPB45wt4)
+   bot.run('MTA4NjE3MDY5Nzc3NDE0MTQ1MA.GOhDCX.4NeNALV7-RyfZaDQPLXPGqVqx1EGtMKNFBZcFM')
