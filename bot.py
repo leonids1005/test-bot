e427aa20bd264df317f9dadb8d4af907273ff2be
leonids1005 <brian960729@gmail.com>
2023-06-02 17:34:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='[', intents = intents)
 
 @bot.event
 async def on_ready():
