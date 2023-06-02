875c96f3deae3c207eaabca076e40eb23386d822
leonids1005 <brian960729@gmail.com>
2023-06-02 16:08:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,14 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
    print(">>Bot is online<<")
 
+@bot.event
+async def on_member_join(member):
+   channel = bot.get_channel(1114098669105451111)
+   await channel.send(F'[member] join!')
+
+@bot.event
+async def on_member_remove(member):
+   channel = bot.get_channel(1114098700000690251)
+   await channel.send(F'[member] leave!')
+
 bot.run('MTA4NjE3MDY5Nzc3NDE0MTQ1MA.GOhDCX.4NeNALV7-RyfZaDQPLXPGqVqx1EGtMKNFBZcFM')
